c851c9e482dd78935ceb2f4c94e3bc3bc4d27e20
haonan92 <haonanzhao92@gmail.com>
2016-11-27 15:00:33 -0500

    done
    I think done haha

---
+++ public/project/views/login/login.js
@@ -0,0 +1,13 @@
+﻿app.controller("LoginCtrl", function ($scope, $http, $location, $rootScope) {
+    $scope.login = function (user) {
+        $http.post("/login", user)
+        .success(function (response) {
+            $rootScope.currentUser = response;
+            $location.url("/profile/" + user.username);
+        })
+        .error(function (error) {
+            $scope.show = true;
+            $scope.errorMessage = 'Unauthorized, Please check your username or password!';
+        });
+    };
+});
---
+++ public/project/views/register/register.js
@@ -0,0 +1,47 @@
+﻿app.controller("RegisterCtrl", function ($scope, $http, $location, $rootScope) {
+    //register method
+    $scope.register = function (user) {
+        console.log(user);
+        if (user.password != user.password2 || !user.password || !user.password2) {
+         //alert("Your passwords don't match");
+            $scope.errorMessage = 'Your passwords do not match';
+            $scope.show = true;
+            $scope.userNameExits = false;
+
+            return;
+      
+        }
+        else {
+            $http.post("/register", user)
+            .success(function (response) {
+                if (response == "user already exists") {
+                   // alert("user already exists!!");
+                    $scope.show = false;
+                    $scope.errorMessage = 'Username already exists!!';
+                    $scope.userNameExits = true;
+
+                }
+                if (response != null && response != "user already exists") {
+                    $rootScope.currentUser = response;
+                    console.log($rootScope.currentUser);
+                    $location.url("/profile");
+                  
+                }
+            });
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
